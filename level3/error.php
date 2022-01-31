97033eed62e5de1a21c604740689800d6512fbf9
CP04042K <cp040529k@gmail.com>
2022-02-01 01:53:54 +0700

    Edit error.php

--- level3/error.php
+++ level3/error.php
@@ -1,6 +1,7 @@
 <?php 
 $content = date("Y/m/d h:i:sa")." - ".urldecode($_SERVER["REQUEST_URI"])."\n";
 
+header("HTTP/1.0 404 Not Found");
 file_put_contents("error_log", $content, FILE_APPEND);
 
 ?>
